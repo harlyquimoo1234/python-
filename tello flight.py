ca2ebc6634300ef4af01f9f33521c86450c34b29
someone <105312769+harlyquimoo1234@users.noreply.github.com>
2022-05-17 10:35:30 -0700

    Update tello flight.py

--- tello flight.py
+++ tello flight.py
@@ -1,4 +1,3 @@
-
 from djitellopy import tello
 from time import sleep
 
@@ -10,22 +9,29 @@ tello = tello.Tello()
 tello.takeoff()
 tello.move_up(120)
 
+
 # move forward 800cm
 tello.move_forword(400)
 tello.move_forword(400)
+sleep(0.5)
 
 # turn left and move 250cm
 tello.rotate_counter_clockwise(90)
+sleep(0.5)
 tello.move_forward(250)
 
 # turn again and move 800cm
 tello.rotate_counter_clockwise(90)
+sleep(0.5)
 tello.move_forward(400)
 tello.move_forword(400)
+sleep(0.5)
 
 # turn left move 250cm
 tello.rotate_counter_clockwise(90)
+sleep(0.5)
 tello.move_forward(250)
+sleep(0.5)
 
 # land
 tello.land(90)
